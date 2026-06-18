8e17643c4cbab43b9617553ab302ce4f723fa34a
oppolise <oppolise@gmail.com>
2026-06-18 12:04:29 +0700

    update life-time

--- life-time/src/main.rs
+++ life-time/src/main.rs
@@ -1,9 +1,15 @@
-fn main() {
-    let treasure;
-    {
-        let local_treasure = String::from("Gold coins");
-        treasure = &local_treasure;
-        println!("{}", treasure)
+fn longest_map<'a>(map1: &'a str, map2: &'a str) -> &'a str {
+    if map1.len() > map2.len() {
+        map1
+    } else {
+        map2
     }
-    // println!("{}", treasure)
+}
+
+fn main() {
+    let m_map1 = "thai thepza";
+    let m_map2 = "thai notthepza";
+
+    let chosen_map = longest_map(m_map1, m_map2);
+    println!("{}", chosen_map);
 }
