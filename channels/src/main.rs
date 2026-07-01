3cb492e4dc111de131498f23809b69296298d2ae
oppolise <oppolise@gmail.com>
2026-07-01 23:08:03 +0700

    add channels plus

--- channels/src/main.rs
+++ channels/src/main.rs
@@ -1,4 +1,7 @@
-use std::sync::{mpsc, Arc};
+use std::{
+    sync::{mpsc, Arc},
+    thread,
+};
 
 fn main() {
     let loots = vec![10, 20, 30];
@@ -6,4 +9,20 @@ fn main() {
 
     let (sender, receiver) = mpsc::sync_channel(3);
     let sender_arc = Arc::new(sender);
+
+    for loot in loots.clone().into_iter() {
+        thread::spawn({
+            let thread_sender = Arc::clone(&sender_arc);
+            move || {
+                thread_sender.send(loot).unwrap();
+            }
+        });
+    }
+
+    for _ in 0..loots.len() {
+        let loot = receiver.recv().unwrap();
+        gold += loot;
+    }
+
+    println!("This is gold now : {}", gold);
 }
